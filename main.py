8e250622e9afe61b45370e61fdae95e8646db2e8
renatormc <renatomartinsrmc@gmail.com>
2025-02-23 20:48:21 -0300

    saving

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from models import db_session, Pericia
 
-def criar_pericias() -> None:
-    for i in range(500):
-        per = Pericia()
-        per.n_celulares = 3
+class RandomNumeroObjetos:
+    def __init__(self):
+        self.stat = {1: 50, 2: 45, 3: 35, 4: 25, 5: 15, 6: 10, 7: 2}
+        self.values = list(self.stat.keys())
+        total = sum(self.stat.values())
+        self.probabilites = map(lambda x: x/total, self.stat.values())
+
+numero_objetos_values = {1: 50, 2: 45, 3: 35, 4: 25, 5: 15, 6: 10, 7: 2}
+
+
+def random_numero_objetos() -> int:
+    values = [1, 2, 3, 4, 5, 6]
+    
