1c9015193bf7756bef6f1f62b957d5d7d6e87fb3
renatormc <renatomartinsrmc@gmail.com>
2025-02-24 17:20:19 -0300

    saving

--- sheets.py
+++ sheets.py
@@ -1,25 +1,58 @@
+from dataclasses import dataclass
 import pandas as pd
 import random
 
+
+@dataclass
+class RandomObjeto:
+    tipo: str
+    subtipo: str
+
+
 class Random:
     def __init__(self) -> None:
-        self.stat = {1: 50, 2: 45, 3: 35, 4: 25, 5: 15, 6: 10, 7: 2}
-        self.values = list(self.stat.keys())
-        total = sum(self.stat.values())
-        self.probabilites = list(map(lambda x: x/total, self.stat.values()))
+        xls = pd.ExcelFile('dados.xlsx')
+
+        df_stat_qtd_objetos = pd.read_excel(xls, 'estatistica_qtd_objetos')
+        self.values_qtd_objetos = list(df_stat_qtd_objetos['Qtd objetos'])
+        total = df_stat_qtd_objetos['Quantidade'].sum()
+        self.probabilites_qtd_objetos = list(map(lambda x: x/total, list(df_stat_qtd_objetos['Quantidade'])))
+
+        df_stat_tipo_objeto = pd.read_excel(xls, 'estatistica_tipo_objeto')
+        self.values_tipo_objeto = list(df_stat_tipo_objeto['Tipo'])
+        total = df_stat_tipo_objeto['Quantidade'].sum()
+        self.probabilites_tipo_objeto = list(map(lambda x: x/total, list(df_stat_tipo_objeto['Quantidade'])))
+
+        df_stat_tipo_extracao = pd.read_excel(xls, 'estatistica_celular')
+        self.values_tipo_extracao = list(df_stat_tipo_extracao['Tipo'])
+        total = df_stat_tipo_extracao['Quantidade'].sum()
+        self.probabilites_tipo_extracao = list(map(lambda x: x/total, list(df_stat_tipo_extracao['Quantidade'])))
+
+        df_stat_capacidade_computador = pd.read_excel(xls, 'estatistica_computador')
+        self.values_capacidade_computador = list(df_stat_capacidade_computador['Capacidade'])
+        total = df_stat_capacidade_computador['Quantidade'].sum()
+        self.probabilites_capacidade_computador = list(map(lambda x: x/total, list(df_stat_capacidade_computador['Quantidade'])))
+
+    def gerar_qtd_objetos(self) -> int:
+        return random.choices(self.values_qtd_objetos, self.probabilites_qtd_objetos)[0]
+
+    def gerar_tipo_objeto(self) -> str:
+        return random.choices(self.values_tipo_objeto, self.probabilites_tipo_objeto)[0]
+
+    def gerar_tipo_extracao(self) -> str:
+        return random.choices(self.values_tipo_extracao, self.probabilites_tipo_extracao)[0]
+
+    def gerar_capacidade_computador(self) -> str:
+        return random.choices(self.values_capacidade_computador, self.probabilites_capacidade_computador)[0]
+
+    def gerar_objeto(self) -> RandomObjeto:
+        tipo = self.gerar_tipo_objeto()
+        subtipo = self.gerar_capacidade_computador() if tipo == "Computador" else self.gerar_tipo_extracao()
+        return RandomObjeto(tipo=tipo, subtipo=subtipo)
 
 
-    def random[T](self, df: pd.DataFrame, col_values: str, col_qtds: str) -> T:
-        values = list(df[col_values])
-        total = df[col_qtds].sum()
-        df['Probabilidade'] = df[col_qtds].apply(lambda x: x/total)
-        probabilites = list(map(lambda x: x/total, list(df[col_qtds])))
-        return  random.choices(values, probabilites)[0]
-
-
-
-xls = pd.ExcelFile('dados.xlsx')
-df_stat_qtd_objetos = pd.read_excel(xls, 'estatistica_qtd_objetos')
 rd = Random()
-print(rd.random[int](df_stat_qtd_objetos, "Qtd objetos", "Quantidade"))
-
+for i in range(100):
+    print(f"\nGerando perícia {i}")
+    for j in range(rd.gerar_qtd_objetos()):
+        print(rd.gerar_objeto())
