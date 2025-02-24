fe144a269647563e752cd54c536c0484de3cffa1
renatormc <renatomartinsrmc@gmail.com>
2025-02-23 22:16:35 -0300

    saving

--- main.py
+++ main.py
@@ -1,14 +1,71 @@
+import random
+
 
 class RandomNumeroObjetos:
     def __init__(self):
         self.stat = {1: 50, 2: 45, 3: 35, 4: 25, 5: 15, 6: 10, 7: 2}
         self.values = list(self.stat.keys())
         total = sum(self.stat.values())
-        self.probabilites = map(lambda x: x/total, self.stat.values())
+        self.probabilites = list(map(lambda x: x/total, self.stat.values()))
 
-numero_objetos_values = {1: 50, 2: 45, 3: 35, 4: 25, 5: 15, 6: 10, 7: 2}
+    def gerar(self) -> int:
+        return random.choices(self.values, self.probabilites)[0]
 
 
-def random_numero_objetos() -> int:
-    values = [1, 2, 3, 4, 5, 6]
-    
+class RandomTipoObjeto:
+    def __init__(self):
+        self.stat = {"celular": 90, "computador": 20}
+        self.values = list(self.stat.keys())
+        total = sum(self.stat.values())
+        self.probabilites = list(map(lambda x: x/total, self.stat.values()))
+
+    def gerar(self) -> str:
+        return random.choices(self.values, self.probabilites)[0]
+
+
+class RandomTipoExtracao:
+    def __init__(self):
+        self.stat = {"fisica": 90, "sistema_arquivos": 20, "logica": 10, "bloqueado": 20, "estragado": 10}
+        self.values = list(self.stat.keys())
+        total = sum(self.stat.values())
+        self.probabilites = list(map(lambda x: x/total, self.stat.values()))
+
+    def gerar(self) -> str:
+        return random.choices(self.values, self.probabilites)[0]
+
+
+class RandomCapacidade:
+    def __init__(self):
+        self.stat = {"500GB": 90, "1024GB": 20, "2048GB": 10}
+        self.values = list(self.stat.keys())
+        total = sum(self.stat.values())
+        self.probabilites = list(map(lambda x: x/total, self.stat.values()))
+
+    def gerar(self) -> str:
+        return random.choices(self.values, self.probabilites)[0]
+
+
+random_tipo = RandomTipoObjeto()
+random_extracao = RandomTipoExtracao()
+random_capacidade = RandomCapacidade()
+
+
+def random_objeto() -> str:
+    tipo = random_tipo.gerar()
+    if tipo == "celular":
+        ext = random_extracao.gerar()
+        return f"{tipo}/{ext}"
+    elif tipo == "computador":
+        cap = random_capacidade.gerar()
+        return f"{tipo}/{cap}"
+    return ""
+
+
+res: dict[str, int] = {}
+for _ in range(500):
+    value = random_objeto()
+    try:
+        res[value] += 1
+    except KeyError:
+        res[value] = 1
+print(res)
