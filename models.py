105d3405fb649df8cb1ea06a0a8d0ec65b85a31e
renatormc <renatomartinsrmc@gmail.com>
2025-03-07 10:42:05 -0300

    saving

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from typing import Literal, Optional
 from unittest.util import strclass
 from sqlalchemy.orm import DeclarativeBase, scoped_session, sessionmaker, mapped_column, Mapped, relationship
 from sqlalchemy.types import TypeDecorator, Float
+from sqlalchemy.ext.hybrid import hybrid_property
 import sqlalchemy as sa
 import config
 from sqlalchemy_utils import observes
@@ -55,7 +56,7 @@ class Pericia(Base):
         return str(self.id)
 
 
-StatusObjeto = Literal["BUFFER", "EXECUTANDO", "AGUARDANDO_PROXIMA_ETAPA", "FINALIZADO"]
+# StatusObjeto = Literal["BUFFER", "EXECUTANDO", "AGUARDANDO_PROXIMA_ETAPA", "FINALIZADO"]
 
 
 class Objeto(Base):
@@ -63,32 +64,36 @@ class Objeto(Base):
     id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
     tipo: Mapped[str] = mapped_column(sa.String(100))
     subtipo: Mapped[str] = mapped_column(sa.String(100))
-    comeco: Mapped[datetime | None] = mapped_column(sa.DateTime)
-    fim: Mapped[datetime | None] = mapped_column(sa.DateTime)
-    status: Mapped[StatusObjeto] = mapped_column(sa.String(100), default="AGUARDANDO_PROXIMA_ETAPA")
-    etapa: Mapped[str | None] = mapped_column(sa.String(100))
+    # status: Mapped[StatusObjeto] = mapped_column(sa.String(100), default="AGUARDANDO_PROXIMA_ETAPA")
+    # etapa: Mapped[str | None] = mapped_column(sa.String(100))
     proxima_etapa: Mapped[str | None] = mapped_column(sa.String(100))
     pericia_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey("pericia.id"))
     pericia: Mapped['Pericia'] = relationship(back_populates="objetos", uselist=False)
-    etapas_: Mapped[str] = mapped_column("etapas", sa.Text)
+    etapas: Mapped[list['Etapa']] = relationship(back_populates="objeto", cascade="all, delete-orphan", order_by="Etapa.order.asc()")
+    
+    
+    # def atualizar_status(self, time: datetime) -> None:
+    #     if self.etapa == None and self.proxima_etapa is None:
+    #         self.status = "FINALIZADO"
+    #     elif self.fim_etapa_corrente is None:
+    #         self.status = "BUFFER"
+    #     elif self.fim_etapa_corrente > time:
+    #         self.status = "EXECUTANDO"
+    #     else:
+    #         self.status = "AGUARDANDO_PROXIMA_ETAPA"
+        
 
-    @property
-    def etapas(self) -> list[str]:
-        return self.etapas_.split("|") if self.etapas_ else []
+    # def etapa_depois(self, etapa: str) -> str | None:
+    #     try:
+    #         return self.etapas[self.etapas.index(etapa) + 1]
+    #     except IndexError:
+    #         return None
 
-    @etapas.setter
-    def etapas(self, value: list[str]) -> None:
-        self.etapas_ = "|".join(value)
-
-    def etapa_depois(self, etapa: str) -> str | None:
-        try:
-            return self.etapas[self.etapas.index(etapa) + 1]
-        except IndexError:
-            return None
-
-    @observes("etapa")
-    def _etapa_changed(self, etapa: str) -> None:
-        self.proxima_etapa = self.etapa_depois(etapa)
+    # @observes("etapa")
+    # def _etapa_changed(self, etapa: str) -> None:
+    #     self.proxima_etapa = self.etapa_depois(etapa)
+        
+      
 
     def __repr__(self):
         return f"{self.tipo} - {self.subtipo}"
@@ -100,28 +105,27 @@ class Equipamento(Base):
     nome: Mapped[str] = mapped_column(sa.String(100))
     buffer: Mapped[int] = mapped_column(sa.Integer)
     capacidade: Mapped[int] = mapped_column(sa.Integer)
-
+    etapas: Mapped[list['Etapa']] = relationship(back_populates="equipamento")
+    
+          
     def __repr__(self):
         return self.nome
-
-
-# EstapaStatus = Literal["PENDENTE", "BUFFER", "EXECUTANDO", "FINALIZADA"]
-
-
-# class Etapa(Base):
-#     __tablename__ = 'etapa'
-#     id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
-#     nome: Mapped[str] = mapped_column(sa.String(100))
-#     duracao: Mapped[timedelta] = mapped_column(TimedeltaAsSeconds)
-#     ordem: Mapped[int] = mapped_column(sa.Integer)
-#     objeto_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey("objeto.id"))
-#     objeto: Mapped['Objeto'] = relationship(back_populates="etapas", uselist=False)
-#     equipamento_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey("equipamento.id"))
-#     equipamento: Mapped['Equipamento'] = relationship(back_populates="etapas", uselist=False)
-
-
-#     def __repr__(self):
-#         return self.nome
-
+    
+    
+class Etapa(Base):
+    __tablename__ = 'etapa'
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    inicio: Mapped[datetime | None] = mapped_column(sa.DateTime)
+    fim: Mapped[datetime | None] = mapped_column(sa.DateTime)
+    order: Mapped[int] = mapped_column(sa.Integer)
+    objeto_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey("objeto.id"))
+    objeto: Mapped['Objeto'] = relationship(back_populates="etapas", uselist=False)
+    equipamento_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey("equipamento.id"))
+    equipamento: Mapped['Equipamento'] = relationship(back_populates="etapas")
+    
+    def __repr__(self):
+        return str(self.id)
+    
 
 Base.metadata.create_all(engine)
+
--- simulation.py
+++ simulation.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from sqlalchemy import and_
+from sqlalchemy import and_, or_
 from typing import Iterable, Iterator
 from models import Equipamento, Objeto, Pericia, db_session
 from models import Perito
@@ -29,46 +29,53 @@ def get_perito_disponivel() -> Perito | None:
 def checar_andamento_equipamento(equipamento: Equipamento, tempo: datetime) -> None:
     query = db_session.query(Objeto).where(
         Objeto.etapa == equipamento.nome,
-        Objeto.status == "EXECUTANDO",
-        Objeto.fim == None
     )
     for obj in query.all():
-        obj.fim = tempo
-        obj.status = "AGUARDANDO_PROXIMA_ETAPA"
+        obj.atualizar_status(tempo)
         db_session.add(obj)
     db_session.commit()
 
-def contar_buffer_equipamento(equipamento: Equipamento) -> int:
+
+def get_finalizadas_equipamento(equipamento: Equipamento, tempo: datetime) -> Iterable[Objeto]:
     query = db_session.query(Objeto).where(
+        Objeto.etapa == equipamento.nome,
+        Objeto.status == "AGUARDANDO_PROXIMA_ETAPA"
+    ).order_by(Objeto.pericia_id)
+    return query.all()
+
+
+# def get_objetos_no_buffer(equipamento: Equipamento) -> Iterable[Objeto]:
+#     query = db_session.query(Objeto).where(
+#         Objeto.etapa == equipamento.nome,
+#         Objeto.status == "BUFFER"
+#     ).order_by(Objeto.pericia_id)
+#     return query.all()
+
+
+def atualizar_numeros_equipamento(equipamento: Equipamento) -> None:
+    equipamento.aguardando = db_session.query(Objeto).where(
         Objeto.etapa == equipamento.nome,
         Objeto.status == "BUFFER"
-    )
-    return query.count()
-
-def contar_executando_equipamento(equipamento: Equipamento) -> int:
-    query = db_session.query(Objeto).where(
+    ).count()
+    equipamento.executando = db_session.query(Objeto).where(
         Objeto.etapa == equipamento.nome,
-        Objeto.status == "EXECUTANDO"
-    )
-    return query.count()
-
-def contar_livre_equipamento(equipamento: Equipamento) -> int:
-    return equipamento.capacidade - contar_executando_equipamento(equipamento) - contar_buffer_equipamento(equipamento)
+        or_(Objeto.status == "EXECUTANDO", Objeto.status == "AGUARDANDO_PROXIMA_ETAPA")
+    ).count()
+    db_session.add(equipamento)
+    db_session.commit()
+    
 
 
 def iniciar_novas_equipamento(equipamento: Equipamento, tempo: datetime) -> None:
-    n_executando = contar_executando_equipamento(equipamento)
-    vagas = equipamento.capacidade - n_executando
+    n = equipamento.capacidade - equipamento.executando
+    if n > equipamento.aguardando:
+        n = equipamento.aguardando
     query = db_session.query(Objeto).where(
         Objeto.etapa == equipamento.nome,
         Objeto.status == "BUFFER"
-    ).limit(vagas).order_by(Objeto.pericia_id)
-    for obj in query.all():
-        print(f"Iniciando objeto {obj.id} no equipamento {equipamento.nome}")
-        obj.status = "EXECUTANDO"
-        obj.comeco = tempo
-        db_session.add(obj)
-    db_session.commit()
+    ).limit(n).order_by(Objeto.pericia_id)
+    for objeto in query.all():
+        etapa = db_session.query(Etapa)
 
 
 
