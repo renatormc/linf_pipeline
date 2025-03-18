dce4a7b0988f26915cea075814902c4ab288d1a3
renatormc <renatomartinsrmc@gmail.com>
2025-03-18 11:31:22 -0300

    saving

--- dev.ps1
+++ dev.ps1
@@ -1,2 +1,17 @@
-$env:PATH="$env:PATH;C:\Program Files\PostgreSQL\17\bin"
+function LodEnvVars {
+    param (
+        [Parameter(Position = 0)]
+        [string]$envFile
+    )
+    if (Test-Path $envFile) {
+        get-content $envFile | ForEach-Object {
+            $name, $value = $_.split('=')
+            set-content env:\$name $value
+        }
+    }
+}
+
+LodEnvVars "$PSScriptRoot\.env"
+
+$env:PATH="$env:PATH;$env:POSTGRES_BIN"
 uv run main.py $args
